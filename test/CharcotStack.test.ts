a36db8a4dcd2ff6558f8314696f238dbdf5c5085
Jose Quijada <joquijada2010@gmail.com>
2021-11-12 09:22:25 -0500

    feat(charcot): save

---
+++ test/CharcotStack.test.ts
@@ -0,0 +1,11 @@
+import { expect, haveResource } from '@aws-cdk/assert'
+import * as sst from '@serverless-stack/resources'
+import CharcotStack from '../stacks/CharcotStack'
+
+test('Charcot Stack', () => {
+  const app = new sst.App()
+  // WHEN
+  const stack = new CharcotStack(app, 'charcot-stack')
+  // THEN
+  expect(stack).to(haveResource('AWS::Lambda::Function'))
+})
