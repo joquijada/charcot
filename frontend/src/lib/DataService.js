5aa97c5e8d5f80f93d8d205bc79804e779e444aa
Jose Quijada <joquijada2010@gmail.com>
2022-07-08 09:40:50 -0400

    feat(search): missing category bug fix
    feat(search): save missing category bug fix

--- frontend/src/lib/DataService.js
+++ frontend/src/lib/DataService.js
@@ -1,4 +1,5 @@
 import { API } from 'aws-amplify'
+import Filter from './Filter'
 
 /*
  * Every time a chart needs to me modified and/or a new one added, add the corresponding coonfig here
@@ -8,7 +9,7 @@ const DIMENSION_CONFIGS = {
     name: 'subjectNumber',
     displayName: 'Subject',
     endpoint: '/cerebrum-images/subjectNumbers',
-    statToDisplay: 'totalCategories',
+    statToDisplay: 'filteredCategoryCount',
     hideInAccordion: true
   },
   age: {
@@ -39,57 +40,101 @@ const calculateTickInterval = (categories) => {
   return Math.pow(10, Math.trunc(Math.log10(avg)))
 }
 
+const retrieveData = async ({ config, dimension, filter }) => {
+  return await API.get('charcot', config.endpoint, {
+    queryStringParameters: {
+      filter: filter.serialize(dimension),
+      numeric: config.isNumeric
+    }
+  })
+}
+
+/**
+ * Produces a map of category name to category data object that includes the
+ * count associated with the category, for example for the Gender dimension:
+ *   Male   -> { count: 12345 }
+ *   Female -> { count: 67890 }
+ */
+const prepareCategoryData = ({ config, dimension, filter, values, resetCountToZero = false }) => {
+  let grandTotal = 0
+  const selectedCategories = new Set()
+  const categories = values.reduce((prev, cur) => {
+    const currentCategory = {
+      count: resetCountToZero ? 0 : cur.count
+    }
+    grandTotal += currentCategory.count
+    if (config.isNumeric) {
+      // Things like 'age' dimension will have the range repeated in the payload, because the ID is the age
+      // but ranges group 2 or more age groups, hence the reason for logic below to keep track of ranges
+      // seen thus far.
+      currentCategory.name = cur.range
+      let existingCategory
+      if ((existingCategory = prev.get(currentCategory.name))) {
+        currentCategory.count += existingCategory.count
+      }
+    } else {
+      currentCategory.name = cur.title
+    }
+
+    prev.set(currentCategory.name, currentCategory)
+
+    if (filter.has({ dimension, category: currentCategory.name })) {
+      selectedCategories.add(currentCategory.name)
+      currentCategory.selected = true
+    }
+
+    return prev
+  }, new Map())
+
+  return {
+    categories,
+    grandTotal,
+    selectedCategories
+  }
+}
+
 class DataService {
   async fetch ({ dimension, filter }) {
     const config = DIMENSION_CONFIGS[dimension]
-    const values = await API.get('charcot', config.endpoint, {
-      queryStringParameters: {
-        filter: filter.serialize(dimension),
-        numeric: config.isNumeric
-      }
+    const filteredValues = await retrieveData({ config, dimension, filter })
+    const { categories: filteredCategories, grandTotal, selectedCategories } = prepareCategoryData({
+      config,
+      dimension,
+      filter,
+      values: filteredValues
     })
-    let grandTotal = 0
-    const selectedCategories = new Set()
-    const categories = values.reduce((prev, cur) => {
-      const currentCategory = {
-        count: cur.count
-      }
-      grandTotal += currentCategory.count
-      if (config.isNumeric) {
-        // Things like 'age' dimension will have the range repeated in the payload, because the ID is the age
-        // but ranges group 2 or more age groups, hence the reason for logic below to keep track of ranges
-        // seen thus far.
-        currentCategory.name = cur.range
-        let existingCategory
-        if ((existingCategory = prev.get(currentCategory.name))) {
-          currentCategory.count += existingCategory.count
-        }
-      } else {
-        currentCategory.name = cur.title
-      }
 
-      prev.set(currentCategory.name, currentCategory)
+    /*
+     * Do a filter-less fetch to get super set of all the dimensions/categories. The filter
+     * might have excluded dimensions/categories yet we need them all available for user selection
+     */
+    let unfilteredCategories = new Map()
+    if (!filter.isEmpty()) {
+      const unfilteredValues = await retrieveData({ config, dimension, filter: new Filter() })
+      ;({ categories: unfilteredCategories } = prepareCategoryData({
+        config,
+        dimension,
+        filter,
+        values: unfilteredValues,
+        resetCountToZero: true
+      }))
+    }
 
-      if (filter.has({ dimension, category: currentCategory.name })) {
-        selectedCategories.add(currentCategory.name)
-        currentCategory.selected = true
-      }
+    const mergedCategories = new Map([...unfilteredCategories, ...filteredCategories])
 
-      return prev
-    }, new Map())
-
-    const totalCategories = Array.from(categories.keys()).length
-    const chartHeight = totalCategories * 30
+    const categoryCount = Array.from(mergedCategories.keys()).length
+    const chartHeight = categoryCount * 30
     return {
       dimension,
       displayName: config.displayName,
-      categories,
+      categories: mergedCategories,
       grandTotal,
       selectedCategories,
       chartHeight: `${chartHeight < 200 ? 200 : (chartHeight > 600 ? 600 : chartHeight)}px`,
-      tickInterval: calculateTickInterval(categories),
+      tickInterval: calculateTickInterval(mergedCategories),
       selectedCategoryCount: selectedCategories.size,
-      totalCategories,
+      categoryCount,
+      filteredCategoryCount: Array.from(filteredCategories.keys()).length,
       statToDisplay: config.statToDisplay,
       hideInAccordion: config.hideInAccordion
     }
