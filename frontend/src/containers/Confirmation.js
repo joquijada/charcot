bf330c0d628c48c1929ac3c1bf0f1ab521171cf7
Jose Quijada <joquijada2010@gmail.com>
2022-10-02 12:40:28 -0400

    feat(confirmation): update confirm message

--- frontend/src/containers/Confirmation.js
+++ frontend/src/containers/Confirmation.js
@@ -8,8 +8,14 @@ class Confirmation extends Component {
   }
 
   render () {
-    return <div className='Login'>
-      <h3>Your request has been submitted, you'll receive an email when its ready</h3>
+    return <div className="Login">
+      <h3>
+        Your request has been submitted, you'll receive an email when its ready
+      </h3>
+      <p>
+        <strong>It will take 20-30 minutes to assemble the files. Depending on the number of files selected, your request might
+        be processed in several chunks, with one email per chunk. Do make sure to check your junk folder.</strong>
+      </p>
     </div>
   }
 }
